bda240006023d8c9dddc8d5152d06d3198059140
krisann stephany <stephany.5@buckeyemail.osu.edu>
2019-04-09 09:14:49 -0400

    Basics
    Basic buttons and upgrades are coded (3)

--- Incremental.py
+++ Incremental.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+#from idlelib.ToolTip import ToolTip as Tip
 
 class Gear:
-    def __init__(self, name, cost, quantity, per_second=0):
+    def __init__(self, name, cost, quantity, per_second=0, limit=0):
         self.gear = name
         self.cost = cost
         self.quantity = quantity
         self.per_second = per_second
+        self.limit = limit
 
 class Clicker:
     def __init__(self, parent):
@@ -14,19 +16,41 @@ class Clicker:
         self.button = tk.Button(parent, text = "Tap", width = 20, height=5, command=self.increment)
         self.current_clicks = 0
         self.gear = {}
-        self.tools = {}
-        self.gear['clicker'] = Gear('clicker', 10, quantity = 1)
+        #self.tools = {}
+        self.gear['clicker'] = Gear('clicker', 10, quantity = 1, limit=100)
+        self.gear['clicker upgrade'] = Gear('clicker upgrade', 50, quantity = 0, limit = 5)
         self.gear['something'] = Gear('something', 20, 0, per_second = 1)
-        self.purchase_buttons['clicker']= tk.Button(parent, text='Hits (%d): 0' % self.gear['clicker'].cost,
+        self.gear['want'] = Gear('want', 100, 0, per_second = 5, limit=20)
+        self.gear['need'] = Gear('need', 200, 0, per_second = 10, limit=10)
+
+        ''' for name, description in (('clicker', 'Hits: (%d): 0'),
+                                  ('clicker upgrade', 'Two handed hit: (%d): 0'),
+                                  ('something', 'Something (%d): 0'),
+                                  ('want', 'You want this (%d): 0'))
+            self.purchase_buttons['name']= tk.Button(parent,
+                                           text=description % self.gear[name].cost,
+                                           command=lamda: x=name: self.purchase(x))
+        '''
+
+        self.purchase_buttons['clicker']= tk.Button(parent, text='Hits: (%d): 0' % self.gear['clicker'].cost,
             command=lambda: self.purchase('clicker'))
+        self.purchase_buttons['clicker upgrade']= tk.Button(parent, text='Two handed hit: (%d): 0' % self.gear['clicker upgrade'].cost,
+            command=lambda: self.purchase('clicker upgrade'))
         self.purchase_buttons['something'] = tk.Button(parent, text = 'Something (%d): 0' % self.gear['something'].cost,
             command=lambda: self.purchase('something'))
+        self.purchase_buttons['want'] = tk.Button(parent, text = 'You want this (%d): 0' % self.gear['want'].cost,
+            command=lambda: self.purchase('want'))
+        self.purchase_buttons['need'] = tk.Button(parent, text = 'You need this (%d): 0' % self.gear['need'].cost,
+            command=lambda: self.purchase('need'))
+
+        #self.tooltips = {'clicker':Tip(self.purchase_buttons['clicker'], 'more clicks'),
+        #                'something':Tip(self.purchase_buttons['something'], 'This is totally something') }
         self.current_click_show = tk.Label(parent, text = '0')
         self.button.grid(row=0, column=0)
-        self.current_click_show.grid(row=1, column=0)
-        manual_row = -1
-        auto_row=-1
-        for name in self.gear:
+        self.current_click_show.grid(row=0, column=1, columnspan=2)
+        manual_row = 0
+        auto_row= 0
+        for name in sorted(self.gear, key=lambda x: self.gear[x].cost):
             if self.gear[name].per_second:
                 manual_row += 1
                 row = manual_row
@@ -40,15 +64,21 @@ class Clicker:
         self.update()
 
     def increment(self):
-        self.current_clicks += self.gear['clicker'].quantity
+        self.current_clicks += self.gear['clicker'].quantity * 2**self.gear['clicker upgrade'].quantity
         self.current_click_show.config(text='%d' % self.current_clicks)
+
     def purchase(self, name):
         if self.current_clicks >= self.gear[name].cost:
+            #print('exceeded limit', self.gear[name].limit, self.gear[name].quantity)
             self.gear[name].quantity += 1
             self.current_clicks -= self.gear[name].cost
+            self.gear[name].cost *= 1.1
             self.current_click_show.config(text = '%d' % self.current_clicks)
             self.purchase_buttons[name].config(
-                text = self.purchase_buttons[name]['text'].split(':')[0] + ': %d' % self.gear[name].quantity)
+                text = self.purchase_buttons[name]['text'].split(': ')[0] + ': {:.1f}: {}'.format(
+                self.gear[name].cost, self.gear[name].quantity))
+            if self.gear[name].limit and self.gear[name].quantity >= self.gear[name].limit:
+                self.purchase_buttons[name].config(state=tk.DISABLED)
 
     def update(self):
         for gear in self.gear.values():
@@ -59,3 +89,12 @@ class Clicker:
 root = tk.Tk()
 clicker = Clicker(root)
 root.mainloop()
+
+
+#To change
+#change names and make an actual story
+#'something' and 'want' button doesn't change the cost but just adds it to the end
+#rescale all costs
+#clicker upgrade button needs to not be so powerful
+#re-enable button after a certain point?
+#can make a for loop for buttons: video 3
