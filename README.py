aeb22ffef099cf46a5e5eb6fc987addcebf7abdb
krisann stephany <stephany.5@buckeyemail.osu.edu>
2019-04-22 17:02:01 -0400

    Read Me revised
    The read me was just revised. That's it. That's all.

--- README.py
+++ README.py
@@ -3,18 +3,18 @@ Krisann Stephany
 Physics 6810 Final Project
 Incremental Game: Puppy Pound
 
-This is an incremental (moreso a decremental) game meant to utilize tkinter. There are 3 files with which I have put different versions of the game so it is easy to view all of the
+This is an incremental (moreso a decremental) game meant to utilize tkinter. There are 4 files with which I have put different versions of the game so it is easy to view all of the
 different outcomes. The files are the following:
 
 Incremental_test.py: the main version of the game
     This file has all popups and normal numbers in the game. This is to say, that events that are in place in the game happen rarely and the game should
     take a decent amount of time to play. That being said, the game is still unfinished and winning in this "normal" mode is not exactly possible/easy yet.
-    The game is not yet balanced, thus, it is highly discouraged to play the game in this original version in the hopes of completing it in it's entirety
+    The game is not yet balanced, thus, it is highly discouraged to play the game in this original version in the hopes of completing it in it's entirety.
     To make the game worthwhile, more buttons would need to be added as well as events and things to do. But so far, this is what has been created. As soon
     as the program is run, a message will pop up. Then the main window will pop up. This will have all the buttons needed to play. Observe the window. After five seconds,
     another window will pop up that will describe the purpose of the game. After this, it is highly encouraged to click the "info" button to see what all the
     other buttons do. Other than that, the game is yours to play.
-Incremental_test2.py: the event version of the game_entry
+Incremental_test2.py: the event version of the game
     This file has all the same concepts as the original, however, events are set to happen much, much more often. This version is just for the user to be able to
     experience all of the events without having to play through the full version of the game. Ideally, more events will get added to the full version in the future,
     but for now, there are six possible events that can affect the game.
@@ -35,5 +35,5 @@ The game has a couple of bugs:
     3. Every once in awhile, messages that are meant to take time before they appear will appear instantly after clicking "ok" on the original messagebox,
         but this may prove to not be an issue.
 
-Overall, I plan to continue to add to the game and create more dependencies between events, fetch, and the decrementation. But for now, this is it.
+Overall, I plan to continue to add to the game and create more dependences between events, fetch, and the decrementation. But for now, this is it.
 """
